3493e6d89d733cb87b8693b5d329b5a7c6679660
Tharlo <hadrien2509@gmail.com>
2023-08-04 19:10:51 +0200

    claptrap done

--- CPP03/ex00/ClapTrap.cpp
+++ CPP03/ex00/ClapTrap.cpp
@@ -6,7 +6,7 @@
 /*   By: hgeissle <hgeissle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 15:29:30 by hgeissle          #+#    #+#             */
-/*   Updated: 2023/08/04 15:56:59 by hgeissle         ###   ########.fr       */
+/*   Updated: 2023/08/04 19:10:30 by hgeissle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 ClapTrap::ClapTrap( void ) : name("ClapTrap"), HP(10), EP(10), AD(0)
 {
-	return ;
+	std::cout << "Default constructor called" << std::endl;
 }
 ClapTrap::ClapTrap( const std::string str) : name(str), HP(10), EP(10), AD(10)
 {
-	return ;
+	std::cout << "Name assignment constructor called" << std::endl;
 }
 ClapTrap::~ClapTrap( void )
 {
-	return ;
+		std::cout << "Destructor called" << std::endl;
 }
 ClapTrap::ClapTrap( const ClapTrap& other ) : name(other.name), HP(other.HP), EP(other.EP), AD(other.AD)
 {
-	return ;
+	std::cout << "Copy constructor called" << std::endl;
 }
 void	ClapTrap::operator=( const ClapTrap& other)
 {
@@ -37,13 +37,32 @@ void	ClapTrap::operator=( const ClapTrap& other)
 
 void ClapTrap::attack(const std::string& target)
 {
+	if (this->HP <= 0 || this->EP <=0)
+		return ;
 	std::cout << "ClapTrap " << this->name << " attacks " << target << ", causing " << this->AD << " points of damage!" << std::endl; 
+	this->EP--;
 }
 void ClapTrap::takeDamage(unsigned int amount)
 {
-	return;
+	if (this->HP <= 0 || this->EP <=0)
+		return ;
+	std::cout << "ClapTrap " << this->name << " gets hurt and loose " << amount << " health points !" << std::endl;
+	this->HP -= amount;
+	if (this->HP <= 0)
+	{
+		std::cout << "ClapTrap " << this->name << " is KO :(" << std::endl;
+		this->HP = 0;
+	}
 }
 void ClapTrap::beRepaired(unsigned int amount)
 {
-	return;
+	if (this->HP <= 0 || this->EP <=0 || amount > 10)
+		return ;
+	if (this->HP == 10)
+		std::cout << "ClapTrap " << this->name << "repairs itself but is already max HP" << std::endl;
+	std::cout << "ClapTrap " << this->name << " repairs itself for an amount of " << amount << " health points" << std::endl;
+	this->HP += amount;
+	this->EP--;
+	if (this->HP > 10)
+		this->HP = 10;
 }
--- CPP03/ex00/main.cpp
+++ CPP03/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: hgeissle <hgeissle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 16:49:50 by hgeissle          #+#    #+#             */
-/*   Updated: 2023/08/04 15:19:23 by hgeissle         ###   ########.fr       */
+/*   Updated: 2023/08/04 19:08:19 by hgeissle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,16 @@
 
 int	main(void)
 {
-	
+	ClapTrap frag("Jack");
+
+	frag.takeDamage(4);
+	frag.attack("mel-faqu");
+	frag.beRepaired(3);
+	frag.takeDamage(5);
+	frag.takeDamage(2);
+	frag.beRepaired(12);
+	frag.beRepaired(10);
+	frag.beRepaired(1);
+	frag.takeDamage(10);
 	return (0);
 }
