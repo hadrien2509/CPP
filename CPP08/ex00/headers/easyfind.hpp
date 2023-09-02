f923accd4db152936ea362becc66cefe8e030dcd
Tharlo <hadrien2509@gmail.com>
2023-09-02 17:54:19 +0200

    .

--- CPP08/ex00/headers/easyfind.hpp
+++ CPP08/ex00/headers/easyfind.hpp
@@ -6,7 +6,7 @@
 /*   By: hgeissle <hgeissle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 16:59:53 by hgeissle          #+#    #+#             */
-/*   Updated: 2023/09/01 17:31:29 by hgeissle         ###   ########.fr       */
+/*   Updated: 2023/09/02 16:52:28 by hgeissle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,19 @@
 # define EASYFIND_HPP
 
 # include <iostream>
+# include <deque>
+# include <list>
+# include <vector>
+
 
 template <typename T>
 
-int		easyfind(T &container, int n)
+typename T::iterator easyfind(T& container, int value)
 {
+	typename T::itearator it = find(container.begin(), container.end(), n);
 	
+	if (it == container.end())
+		throw 
 }
+
+#endif
--- CPP08/ex00/srcs/main.cpp
+++ CPP08/ex00/srcs/main.cpp
@@ -6,7 +6,23 @@
 /*   By: hgeissle <hgeissle@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/01 16:59:34 by hgeissle          #+#    #+#             */
-/*   Updated: 2023/09/01 16:59:35 by hgeissle         ###   ########.fr       */
+/*   Updated: 2023/09/02 16:48:43 by hgeissle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../headers/easyfind.hpp"
+
+int main(void)
+{
+	std::vector<int>	v;
+	std::list<int>		l;
+	std::deque<int>		d;
+
+	v.push_back(1);
+	v.push_back(2);
+	v.push_back(3);
+	v.push_back(4);
+	v.push_back(5);
+
+	easyfind(v, 4);
+}
